7574faf8d94fa870625db6e7837327a961e760b2
MatthiasTheys <r0895598@studnet.thomasmore.be>
2022-11-20 20:47:10 +0100

    changes dockerfile

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
 @app.get("/back")
 async def get_backexercise():
     #extract data from exercises.json for back exercises
-    with open('app/exercises.json') as f:
+    with open('exercises.json') as f:
         data = json.load(f)
     back = data['back']
     return back[str(randint(1, len(back)))]["name"]
@@ -33,14 +33,14 @@ async def get_backexercise():
 @app.get("/chest")
 async def get_chestexercise():
     #extract chest exercises from exercises.json and return the name of the first one
-    with open('app/exercises.json') as f:
+    with open('exercises.json') as f:
         data = json.load(f)
     chest = data['chest']
     return chest[str(randint(1, len(chest)))]["name"]
 @app.post("/group/{group}")
 async def make_group(group: str):
     #check if group is back or chest and return the corresponding exercise
-    with open('app/exercises.json') as f:
+    with open('exercises.json') as f:
         data = json.load(f)
     if group == 'back':
         back = data['back']
