a48487a8bfab72f57438369e16f596442ec191d1
MatthiasTheys <r0895598@studnet.thomasmore.be>
2022-11-20 20:05:47 +0100

    Cors-origins

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from random import randint
 import json
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:8080",
+    "https://matthiastheys.github.io"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/back")
